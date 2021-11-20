63b292d2b9996f7e4455a0c37cedcb14499ac5ea
Ko <kolton.cktam@gmail.com>
2021-11-20 20:42:13 +0800

    Added Gesture Recognition code

---
+++ main.py
@@ -0,0 +1,29 @@
+# 3844.py
+import tkinter as tk
+import pygubu
+from gesreg import *
+
+
+class HelloWorldApp:
+    
+    def __init__(self):
+
+        #1: Create a builder
+        self.builder = builder = pygubu.Builder()
+
+        #2: Load an ui file
+        builder.add_from_file('buttons.ui')
+
+        #3: Create the mainwindow
+        self.mainwindow = builder.get_object('mainwindow')
+        
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == '__main__':
+    app = HelloWorldApp()
+    app.run()
+
+def leaveApp():
+    exit()
