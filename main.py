e501a8355d2767ece73e552df42464261fa172f1
Villager641 <m4a79x@icloud.com>
2021-11-22 11:57:15 +0800

    UI update

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-# 3844.py
+import pathlib
 import tkinter as tk
+import tkinter.ttk as ttk
 import pygubu
-from gesreg import *
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "buttons.ui"
 
 
-class HelloWorldApp:
-    
-    def __init__(self):
-
-        #1: Create a builder
+class ButtonsApp:
+    def __init__(self, master=None):
         self.builder = builder = pygubu.Builder()
-
-        #2: Load an ui file
-        builder.add_from_file('buttons.ui')
-
-        #3: Create the mainwindow
-        self.mainwindow = builder.get_object('mainwindow')
-        
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        self.mainwindow = builder.get_object('toplevel1', master)
+        builder.connect_callbacks(self)
+    
     def run(self):
         self.mainwindow.mainloop()
 
+    def btn1_callback()(self, event=None):
+        pass
+
+    def (self, event=None):
+        pass
+
+    def btn2_callback()(self, event=None):
+        pass
+
+    def btn3_callback()(self, event=None):
+        pass
+
 
 if __name__ == '__main__':
-    app = HelloWorldApp()
+    app = ButtonsApp()
     app.run()
 
-def leaveApp():
-    exit()
